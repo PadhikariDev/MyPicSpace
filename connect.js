6ffc6f99c2b35b62decfe5afa836a2f52fb6a6b3
PadhikariDev <prashant.kyler99@gmail.com>
2025-05-08 23:51:49 +0530

    setup-1

--- connect.js
+++ connect.js
@@ -6,3 +6,5 @@ import mongoose from "mongoose";
 function conenctMD(url) {
     return mongoose.connect(url);
 }
+
+export default conenctMD
--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 
 import express from "express";
 import path from "path";
+import { fileURLToPath } from "url";
+import conenctMD from "./connect.js";
 
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PORT = 3000;
 const app = express();
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.urlencoded());
-
-
+app.get("/", (req, res) => {
+    res.render("login")
+})
+app.get("/api/signup", (req, res) => {
+    res.render("signup")
+})
+conenctMD("mongodb://127.0.0.1:27017/myPicSpace").then(() => {
+    console.log("Connection to Database has been established.")
+}).catch(err => {
+    console.error("Error connecting to the DataBase.");
+})
 
 
 
